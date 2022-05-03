5affb5a98e47d90a44901d26c6aeb5468d20c6f9
Arsalan Temori <arslan.temori159@gmail.com>
2022-05-03 13:18:43 +0200

    fertigeVersion2

--- src/PrintTodo.js
+++ src/PrintTodo.js
@@ -1,12 +1,52 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 const PrintTodo = (props) => {
+  let backgroundColor = "white";
+  switch (props.priority) {
+    case 1:
+      backgroundColor = "red";
+      break;
+    case 2:
+      backgroundColor = "orange";
+      break;
+    case 3:
+      backgroundColor = "black";
+      break;
+  }
   return (
-    <div class="ui inverted segment">
-      <div class="ui inverted relaxed divided list">
-        <div class="item">
-          <div class="header">{props.text}</div>
+    <div className="ui inverted segment">
+      <div
+        className="ui inverted relaxed divided list"
+        style={{ color: "red" }}
+      >
+        <div className="item">
+          <div className="header">
+            <div
+              className="prioritätKreis"
+              style={{
+                height: "15px",
+
+                width: "15px",
+                backgroundColor,
+                borderColor: "white",
+                border: "3px solid white",
+                borderRadius: "50%",
+                position: "absolute",
+                left: "5px",
+                top: "40%",
+              }}
+            ></div>
+            {props.text}
+            {props.priority}
+
+            <button
+              onClick={() => props.onDelete(props.index)}
+              className="ui compact icon button"
+              style={{ marginLeft: "10px" }}
+            >
+              <i className="times circle outline icon"></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -6,17 +6,43 @@ class App extends React.Component {
   state = {
     value: "",
     list: [],
+    priority: 3,
+  };
+
+  handleDelete = (index) => {
+    var list2 = this.state.list;
+    list2.splice(index, 1);
+    this.setState({ list: list2 });
   };
 
   getInput = (event) => {
     this.setState({ value: event.target.value });
   };
 
-  printValue = () => {
-    this.setState({
-      list: [...this.state.list, this.state.value],
-      value: "",
-    });
+  printValue = (e) => {
+    e.preventDefault();
+
+    if (this.state.value === "") {
+      alert("Fehler");
+    } else {
+      this.setState({
+        list: [
+          ...this.state.list,
+          { title: this.state.value, priorität: this.state.priority },
+        ],
+        value: "",
+      });
+      console.log(this.state.Priority);
+    }
+  };
+  priority1 = () => {
+    this.setState({ priority: 1 });
+  };
+  priority2 = () => {
+    this.setState({ priority: 2 });
+  };
+  priority3 = () => {
+    this.setState({ priority: 3 });
   };
 
   render() {
@@ -34,33 +60,56 @@ class App extends React.Component {
           borderRadius: "25px",
         }}
       >
+        {console.log(this.state.priority)}
         <div style={{ textAlign: "center", margin: "0 auto" }}>
           <h2 className="ui icon header">
             <i className="list alternate outline icon"></i>
             <div className="content">
               MY LIST
               <div className="sub header">Füge etwas deiner Liste hinzu</div>
+              <button className="ui red basic button" onClick={this.priority1}>
+                Priorität 1
+              </button>
+              <button
+                className="ui orange basic button"
+                onClick={this.priority2}
+              >
+                Priorität 2
+              </button>
+              <button
+                className="ui black basic button"
+                onClick={this.priority3}
+              >
+                Priorität 3
+              </button>
             </div>
           </h2>
 
           <p></p>
-          <div className="ui input focus">
-            <input
-              type="text"
-              value={this.state.value}
-              placeholder="Eingabe"
-              onChange={this.getInput}
-              style={{ marginRight: "10%" }}
+          <form onSubmit={this.printValue}>
+            <div className="ui input focus">
+              <input
+                type="text"
+                value={this.state.value}
+                placeholder="Eingabe"
+                onChange={this.getInput}
+                style={{ marginRight: "10%" }}
+              />
+              <button
+                className="ui inverted primary button"
+                onClick={this.printValue}
+              >
+                Hinzufügen
+              </button>
+            </div>
+          </form>
+          {this.state.list.map((item, index) => (
+            <PrintTodo
+              text={item.title}
+              priority={item.priorität}
+              index={index}
+              onDelete={this.handleDelete}
             />
-            <button
-              className="ui inverted primary button"
-              onClick={this.printValue}
-            >
-              Hinzufügen
-            </button>
-          </div>
-          {this.state.list.map((item) => (
-            <PrintTodo text={item} />
           ))}
         </div>
       </div>
